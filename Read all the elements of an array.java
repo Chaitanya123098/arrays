e5be076266acd69957279469c47f1380f992585e
Chaitanya123098 <65679501+Chaitanya123098@users.noreply.github.com>
2022-10-21 17:45:18 +0530

    Add files via upload

---
+++ Read all the elements of an array.java
@@ -0,0 +1,17 @@
+class Arrayed {
+    
+    public static void printelements(int[] arr){
+        for(int i=0;i<arr.length;i++){
+            
+            System.out.print(arr[i]+" ");
+            
+        }
+    }
+
+    public static void main(String[] args) {
+      
+       int arr[]={2,4,5,6,7,9};
+       printelements(arr);
+       
+    }
+}
